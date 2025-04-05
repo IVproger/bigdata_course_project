5ee5271780f5c4bbc9344b09c4c8fe71d45e00a5
Ivan Golov <i.golov@innopolis.university>
2025-04-05 20:21:52 +0300

    stage 1 complete

--- sql/test_database.sql
+++ sql/test_database.sql
@@ -1,8 +1,1 @@
--- Check the number of rows in the table
-SELECT COUNT(*) AS total_rows FROM job_descriptions;
-
--- View a few sample rows
-SELECT * 
-FROM job_descriptions 
-ORDER BY id 
-LIMIT 5;
+SELECT * FROM job_descriptions ORDER BY id LIMIT 1;
