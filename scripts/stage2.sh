8115026ac520d5bb1bc9f84150d31b6a0c7c063b
Ivan Golov <i.golov@innopolis.university>
2025-04-20 12:33:04 +0300

    run stage2 need to exit py files

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -30,7 +30,7 @@ echo "Executing Hive queries, storing results in PostgreSQL, and exporting to CS
 # Note: Adjust master, driver memory, executor memory/cores as needed for your cluster
 spark-submit \
     --master yarn \
-    --deploy-mode cluster \
+    --deploy-mode client \
     --jars /shared/postgresql-42.6.1.jar \
     scripts/run_hive_queries.py
 
