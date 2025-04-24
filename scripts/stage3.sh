c0c7c3cb85dd79f3e7d2c453d61dce2ebe6ecf7c
Ivan Golov <i.golov@innopolis.university>
2025-04-24 21:30:01 +0300

    data_predprocessing.py done for stage3 automation

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -4,6 +4,13 @@ set -e  # Stop script on any error
 # 1. Create local data directory (if not exists)
 mkdir -p data
 
+export YARN_CONF_DIR=/etc/hadoop/conf
+echo "Set YARN_CONF_DIR to $YARN_CONF_DIR"
+
+export PYSPARK_PYTHON=python3.11
+export PYSPARK_DRIVER_PYTHON=python3.11
+
+
 # 2. Clean HDFS directory
 echo "Cleaning HDFS /project/data..."
 hdfs dfs -rm -r project/data >/dev/null 2>&1 || true  # Ignore errors if directory doesn't exist
@@ -13,19 +20,19 @@ echo "Recreating HDFS structure..."
 hdfs dfs -mkdir -p project/data/train
 hdfs dfs -mkdir -p project/data/test
 
-# 4. Data preparation...
-# echo "Data preparation..."
-# spark-submit \
-#     --master yarn \
-#     --deploy-mode client \
-#     scripts/run_hive_queries.py
+echo "Data preparation..."
+spark-submit \
+    --master yarn \
+    --deploy-mode client \
+    --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=python3.11 \
+    --conf spark.executorEnv.PYSPARK_PYTHON=python3.11 \
+    scripts/data_predprocessing.py
 
+# 4. Copy files from HDFS to local (with merging)
+echo "Downloading training data..."
+hdfs dfs -getmerge project/data/train/*.json data/train.json
 
-# # 4. Copy files from HDFS to local (with merging)
-# echo "Downloading training data..."
-# hdfs dfs -getmerge project/data/train/*.json data/train.json
+echo "Downloading test data..."
+hdfs dfs -getmerge project/data/test/*.json data/test.json
 
-# echo "Downloading test data..."
-# hdfs dfs -getmerge project/data/test/*.json data/test.json
-
-# echo "Operation completed successfully!"
+echo "Operation completed successfully!"
